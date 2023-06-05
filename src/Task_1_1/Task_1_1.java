663c0dd428b02bf69544837ab803d38953f19a6a
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-06-05 17:32:10 +0300

    1

--- src/Task_1_1/Task_1_1.java
+++ src/Task_1_1/Task_1_1.java
@@ -1,5 +1,5 @@
 	
-// 1. Дан массив названий переменных в camelCase. Преобразовать названия в snake_case.
+// 1. Дан массив названий переменных в camelCase. Преобразовать названия в snake_case
 	
 package Task_1_1;
 	
@@ -7,29 +7,43 @@ public class Task_1_1 {
 	
 	public static void main(String[] args) {
 	
-	System.out.println("-------------------------------------------------------------------------------");
-	System.out.println("ПРОГРАММА : ПРЕОБРАЗОВАНИЕ МАССИВА НАЗВАНИЙ ПЕРЕМЕННЫХ ИЗ CAMELCASE В SNAKECASE");
-	System.out.println("-------------------------------------------------------------------------------");
+	System.out.println("--------------------------------------------------------------------------------------------");
+	System.out.println("ПРОГРАММА : ПРЕОБРАЗОВАНИЕ МАССИВА НАЗВАНИЙ ПЕРЕМЕННЫХ ИЗ camelCase И CamelCase В snake_case");
+	System.out.println("--------------------------------------------------------------------------------------------");
 	
-	// Массив переменных в camelCase
-	String [] camelCase = {"getChars", "equalsIgnoreCase", "toLowerCase",
-			 				"toUpperCase", "toCharArray", "charAt"};
+	// Массив переменных в camelCase И CamelCase
+	String [] camelCase = { "HumpBackNotation", "getChars", "BackColor", "charAt", "CamelCase", "equalsIgnoreCase", "CamelHumpedWord", "toLowerCase",
+			"MixedCase", "toUpperCase", "WordsStrungTogether", "mixedCase", "WordsRunTogether", "toCharArray", "WordCase", "backColor"};
 	
-	System.out.println("--------------------------------------------------------------------------------------------------------");
-	
-	System.out.println("Массив переменных в camelCase : " + String.join(" , ", camelCase));
-	System.out.println("--------------------------------------------------------------------------------------------------------");
+	// System.out.println("--------------------------------------------------------------------------------------------------------");
+	// System.out.println("Массив переменных в camelCase и CamelCase : " + String.join(" , ", camelCase));
+	// System.out.println("--------------------------------------------------------------------------------------------------------");
 	
 	Task_1_1_res Input = new Task_1_1_res();
 	
-	
 	// Массив для переменных в snake_case
-	String [] snakeCase = Input.toSnakeCase(camelCase);
+	String [] snakeCase = Input.to_snake_case(camelCase);
 	
-	System.out.println("--------------------------------------------------------------------------------------------------------");
+	//System.out.println("--------------------------------------------------------------------------------------------------------");
+	//System.out.println("Массив переменных в snake_case : " + String.join(" , ", snakeCase));
+	//System.out.println("--------------------------------------------------------------------------------------------------------");
 	
-	System.out.println("Массив переменных в snake_case : " + String.join(" , ", snakeCase));
-	System.out.println("--------------------------------------------------------------------------------------------------------");
+	
+		if ( camelCase.length == snakeCase.length ) {
+			
+			System.out.println("          МАССИВ НАЗВАНИЯ ПЕРЕМЕННЫХ :");
+			System.out.println("--------------------------------------------------");
+			System.out.println("camelCase И CamelCase :  " + "   " + "snake_case : " );
+			
+			for (int i = 0; i < camelCase.length; i++ ) {
+				
+				System.out.println("--------------------------------------------------");
+				System.out.printf("%-20s  :     %-20s%n", camelCase[i], snakeCase[i]);
+			}
+			
+			System.out.println("--------------------------------------------------");
+			
+		}
 	
 	}
 	
--- src/Task_1_1/Task_1_1_res.java
+++ src/Task_1_1/Task_1_1_res.java
@@ -4,46 +4,52 @@ package Task_1_1;
 public class Task_1_1_res {
 	
 /*
-	CamelCase (с англ. — «ВерблюжийРегистр», также «ГорбатыйРегистр», «СтильВерблюда») —
+	CamelCase : camelCase И CamelCase (с англ. — «ВерблюжийРегистр», также «ГорбатыйРегистр», «СтильВерблюда») —
 	стиль написания составных слов, при котором несколько слов пишутся слитно без пробелов,
 	при этом каждое слово внутри фразы пишется с прописной буквы. Стиль получил название CamelCase,
 	поскольку прописные буквы внутри слова напоминают горбы верблюда (англ. Camel).
 */
 /*
-	Snake case (или snake_case, рус. змеиный_регистр) — стиль написания составных слов,
+	Snake case : snake_case (или snake_case, рус. змеиный_регистр) — стиль написания составных слов,
 	при котором несколько слов разделяются символом подчеркивания (_), и не имеют пробелов в записи,
 	причём каждое слово обычно пишется с маленькой буквы — «foo_bar», «hello_world» и т. д.
 	Такой стиль написания используется для именования переменных и функций в исходном коде,
 	и иногда для именования файлов на компьютере.
 */
 	
-	//ПРЕОБРАЗОВАНИЕ МАССИВА НАЗВАНИЙ ПЕРЕМЕННЫХ ИЗ CAMELCASE В SNAKECASE
-	public String [] toSnakeCase (String [] camelCase) {
+	//ПРЕОБРАЗОВАНИЕ МАССИВА НАЗВАНИЙ ПЕРЕМЕННЫХ ИЗ camelCase И CamelCase В snake_case
+	public String [] to_snake_case (String [] CamelCase) {
 		
-		String [] snakeCase = new String[camelCase.length];
+		String [] snake_case = new String[CamelCase.length];
 		
-		for (int i = 0; i < snakeCase.length; i++) {
+		for (int i = 0; i < snake_case.length; i++) {
 			
 			StringBuilder strBuilder = new StringBuilder();
 			
-			for (int j = 0; j < camelCase[i].length(); j++) {
+			for (int j = 0; j < CamelCase[i].length(); j++) {
 				
-				char letter = camelCase[i].toCharArray()[j];
+				char char1 = CamelCase[i].toCharArray()[j];
 				
-				if (Character.isLowerCase(letter)) {
+				if (Character.isLowerCase(char1)) {
 					
-					strBuilder.append(letter);
+					strBuilder.append(char1);
 					
 				} else {
 					
-					strBuilder.append("_");
-					
-					strBuilder.append(Character.toLowerCase(letter));
-					
+					if ( j > 0 ) {
+						
+						strBuilder.append("_");
+						
+						strBuilder.append(Character.toLowerCase(char1));
+						}
+						
+					if ( j == 0 ) {
+						strBuilder.append(Character.toLowerCase(char1));
+						}
 				}
 			}
-			snakeCase[i] = strBuilder.toString();
+			snake_case[i] = strBuilder.toString();
 		}
-		return snakeCase;
+		return snake_case;
 	}
 }
