34d9dac02f3fa8388f21b06b668d0899d6c3b02d
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-06-14 19:54:09 +0300

    1

---
+++ src/Task_2_6/Task_2_6_res.java
@@ -0,0 +1,35 @@
+	
+package Task_2_6;
+	
+public class Task_2_6_res {
+	
+	// Вывод на печать строки
+	public void Print_line(String line) {
+		
+		for (int i = 0; i < line.length(); i++) {
+			
+				System.out.print(line.charAt(i));
+			}
+			
+		}
+	
+	
+	// ПОЛУЧЕНИЕ ИЗ ЗАДАННОЙ СТРОКИ НОВОЙ, ПОВТОРИВ КАЖДЫЙ СИМВОЛ ДВАЖДЫ
+	public String new_line(String line) {
+		
+		StringBuilder strBuilder = new StringBuilder();
+		
+		for (int i = 0; i < line.length(); i++ ) {
+			
+			strBuilder.append(line.charAt(i));
+			
+			strBuilder.append(line.charAt(i));
+			
+		}
+		
+		String new_line = strBuilder.toString();
+		
+		return new_line;
+	}
+	
+}
